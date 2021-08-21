ecd5659b72e9b18890262ac15355d8753183f1c6
AhmadSupriyatna <ahmadsupriyatna1@gmail.com>
2021-08-21 20:36:49 +0700

    Tugas 8 Javascript
    Tugas ini kami buat untuk niomic

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+const tugasDelapan=()=>{
+    var monitor={
+        merek:'Dell',
+        ukuran: '24"',
+        jenis: 'led',
+    }
+    console.log(monitor);
+    monitor.merek='Asus';
+    console.log(monitor);
+            
+    }
+tugasDelapan();
